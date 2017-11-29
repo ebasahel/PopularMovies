a0cc6329ac3c321d53899104982e658f6b9bb3f8
emanbasahel <Eman0500821725>
2017-11-29 14:21:41 +0300

    init

---
+++ app/src/main/java/sa/thiqah/emanbasahel/popularmovies_1/views/MainActivity.java
@@ -0,0 +1,15 @@
+package sa.thiqah.emanbasahel.popularmovies_1.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sa.thiqah.emanbasahel.popularmovies_1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
